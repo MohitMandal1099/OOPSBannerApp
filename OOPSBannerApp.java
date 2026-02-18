12af698364dacc4ab56e360aa8b4dd2058a89830
MohitMandal1099 <https://github.com/MohitMandal1099/OOPSBannerApp.git>
2026-02-18 15:25:57 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This class prints "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
